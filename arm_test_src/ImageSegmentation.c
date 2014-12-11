468049664b9f874937b1362a74e939bad5533d7f
eonian <quyingze@qq.com>
2014-12-11 20:39:06 +0800

    keil下测试源码
    姚导的板子时间测试代码

---
+++ arm_test_src/ImageSegmentation.c
@@ -0,0 +1,284 @@
+#include "ImageSegmentation.h"
+
+#define INVALID_EDGE (MAXNODE * 12)
+#define INVALID_NODE (MAXPIXEL + 3)
+UINT8T image_Canny[SIZE];
+extern INT32T h, w;
+extern INT32T numLabels;
+extern INT32T i, j, k;
+INT32T idx, t;
+
+EDGE a[MAXNODE * 8];
+INT32T f[MAXNODE], pre[MAXNODE], lv[MAXNODE], gap[MAXNODE], cur[MAXNODE];
+INT32T tot;
+INT32T N = MAXPIXEL + 2;
+INT32T source, sink;
+extern INT32T lb[SIZE];
+
+INT32T pixelEdgeS, pixelEdgeE;
+INT32T sourceEdgeS, sourceEdgeE;
+INT32T sinkEdgeS, sinkEdgeE;
+RGBTYPE image_Correction[SIZE];
+
+INT32T max2(INT32T a, INT32T b){
+	return (a > b) ? a : b;
+}
+
+INT32T max3(INT32T a, INT32T b, INT32T c){
+	return max2(max2(a, b), c);
+}
+
+INT32T RGB2Gray(RGBTYPE p){
+	return (INT32T)(p.r * 0.299 + p.g * 0.587 + p.b * 0.114);
+}
+
+INT32T Index(INT32T i, INT32T j){
+	return i * w + j;
+}
+
+INT32T ComputeDataCost(INT32T idx, INT32T l){
+	INT32T gray, cost;
+	gray = RGB2Gray(image_Correction[idx]);
+	switch (l)
+	{
+	case 0: {cost = max2(gray - 10, 0) + 80; break;}
+	case 1: {cost = max2(210 - gray, 0) + 10; break;}
+	case 2: {if (image_Canny[idx] > 0) cost = 1; else cost = max2(max3(image_Correction[idx].r, image_Correction[idx].g, image_Correction[idx].b), max3(255 - image_Correction[idx].r, 255 - image_Correction[idx].g, 255 - image_Correction[idx].b)) * 7 / 10;  break; }
+	default: {cost = 0; break; }
+	}
+	//printf("(r=%d g=%d b=%d gray=%d) l=%d cost=%d\n", image_Correction[idx].r, image_Correction[idx].g, image_Correction[idx].b, gray, l, cost);
+	return cost;
+}
+
+INT32T ComputeSmoothCost(INT32T l1, INT32T l2){
+	INT32T varity = 2;
+	if (l1 == l2) return 0;
+	if (l1 == 0){
+		if (l2 == 1) return 120 / varity;
+		if (l2 == 2) return 60 / varity;
+	}
+	else
+	if (l1 == 1){
+		if (l2 == 2) return 60 / varity;
+		if (l2 == 0) return 120 / varity;
+	}
+	else
+	if (l1 == 2){
+		if (l2 == 0) return 60 / varity;
+		if (l2 == 1) return 60 / varity;
+	}
+	return 0;
+}
+
+void AddEdge(INT32T x, INT32T y, INT32T cap, INT32T revCap){
+	a[tot].x = x;
+	a[tot].y = y;
+	a[tot].cap = cap;
+	a[tot].next = f[x];
+	f[x] = tot;
+	tot++;
+
+	a[tot].x = y;
+	a[tot].y = x;
+	a[tot].cap = revCap;
+	a[tot].next = f[y];
+	f[y] = tot;
+	tot++;
+}
+
+
+
+
+void InitMap(){
+	N = MAXPIXEL + 2;
+	for (i = 0; i < N; i++) f[i] = INVALID_EDGE;
+	tot = 0;
+	source = MAXPIXEL + 1;
+	sink = MAXPIXEL + 2;
+}
+
+void BuildMap(){
+	sourceEdgeS = tot;
+	sourceEdgeS = tot;
+	for (i = 0; i < h; i++)
+	for (j = 0; j < w; j++) AddEdge(source, Index(i, j), 0, 0);
+	sourceEdgeE = tot;
+
+	sinkEdgeS = tot;
+	for (i = 0; i < h; i++)
+	for (j = 0; j < w; j++) AddEdge(Index(i, j), sink, 0, 0);	
+	sinkEdgeE = tot;
+
+	pixelEdgeS = tot;
+	for (i = 0; i < h; i++)
+	for (j = 0; j < w; j++){
+		if (i > 0) AddEdge(Index(i - 1, j), Index(i, j), 0, 0);
+		if (j > 0) AddEdge(Index(i, j - 1), Index(i, j), 0, 0);
+	}
+	pixelEdgeE = tot;
+}
+
+INT32T ComputeEnergy(){
+	INT32T energy = 0;
+	for (i = 0; i < MAXPIXEL; i++) energy += ComputeDataCost(i, lb[i]);
+	for (i = 0; i < MAXPIXEL; i++) energy += (int)ComputeDataCost(i, lb[i]);
+	for (t = pixelEdgeS; t < pixelEdgeE; t += 2) energy += ComputeSmoothCost(lb[a[t].x], lb[a[t].y]);
+	return energy;
+}
+
+
+void ModifyMap(INT32T alpha, INT32T beta){
+	for (t = 0; t < tot; t++) a[t].cap = 0;
+	for (t = sourceEdgeS; t < sourceEdgeE; t += 2){
+			if ((lb[a[t].y] == alpha) || (lb[a[t].y] == beta))  a[t].cap = ComputeDataCost(a[t].y, alpha);
+	}
+
+	for (t = sinkEdgeS; t < sinkEdgeE; t += 2){
+			if ((lb[a[t].x] == alpha) || (lb[a[t].x] == beta))  a[t].cap = ComputeDataCost(a[t].x, beta);
+	}
+
+	for (t = pixelEdgeS; t < pixelEdgeE; t ++){
+		if ((lb[a[t].x] == alpha) || (lb[a[t].x] == beta)){
+			if ((lb[a[t].y] == alpha) || (lb[a[t].y] == beta)){
+				a[t].cap = ComputeSmoothCost(alpha, beta);
+			}
+		}
+	}
+}
+
+INT32T Maxflow(){
+	INT32T flow = 0, aug;
+	INT32T x, y;
+	UINT8T flag;
+	INT32T minLv;
+	for (i = 0; i < N; i++) pre[i] = INVALID_NODE;
+	for (i = 0; i < N; i++) cur[i] = f[i];
+	
+	for (i = 0; i < N; i++) lv[i] = 1;
+	lv[sink] = 0;
+	for (i = 0; i < N; i++) gap[i] = 0;
+	gap[1] = N - 1;
+	gap[0] = 1;
+
+	x = source;
+	while (lv[x] < N){
+		flag = 0;
+		for (t = cur[x]; t != INVALID_EDGE; t = a[t].next){
+			y = a[t].y;
+			cur[x] = t;
+			if (a[t].cap > 0 && lv[x] == lv[y] + 1){
+				flag = 1;
+				pre[y] = x;
+				x = y;
+				if (x == sink){
+					aug = INF;
+					y = pre[x];
+					while (1){
+						if (a[cur[y]].cap < aug) aug = a[cur[y]].cap;
+						if (y == source) break;
+						y = pre[y];
+					}
+					flow += aug;
+					y = pre[x];
+					while (1){
+						a[cur[y]].cap -= aug;
+						a[cur[y] ^ 1].cap += aug;
+						if (y == source) break;
+						y = pre[y];
+					}
+					x = source;
+				}
+				break;
+			}
+		}
+		if (flag) continue;
+		minLv = N;
+		for (t = f[x]; t != INVALID_EDGE; t = a[t].next){
+			y = a[t].y;
+			if (a[t].cap > 0 && lv[y] < minLv){
+				minLv = lv[y];
+				cur[x] = t;
+			}
+		}
+		if ((--gap[lv[x]]) == 0) break;
+		lv[x] = minLv + 1;
+		gap[lv[x]]++;
+		if (x != source) x = pre[x];
+	}
+	return flow;
+}
+void TestMaxflow(){
+	InitMap();
+	N = 6;
+	source = 0;
+	sink = N - 1;
+	AddEdge(source, 1, 100, 0);
+	AddEdge(1, 2, 110, 0);
+	AddEdge(2, sink, 20, 0);
+	AddEdge(source, 3, 30, 0);
+	AddEdge(3, 4, 200, 0);
+	AddEdge(4, sink, 201, 0);
+	AddEdge(2, 4, 11, 0);
+	AddEdge(4, 2, 30, 0);
+	Maxflow();
+}
+
+void ChangeLabel(int alpha, int beta){
+	for (t = sinkEdgeS; t < sinkEdgeE; t += 2) if (a[t].cap == 0 && a[t ^ 1].cap > 0) lb[a[t].x] = beta;
+	for (t = sourceEdgeS; t < sourceEdgeE; t += 2) if (a[t].cap == 0 && a[t ^ 1].cap > 0) lb[a[t].y] = alpha;
+}
+
+void AlphaBetaSwap(){
+	INT32T alpha, beta;
+	for (alpha = 0; alpha < numLabels; alpha++)
+	for (beta = alpha + 1; beta < numLabels; beta++){
+		ModifyMap(alpha, beta);
+		Maxflow();
+		ChangeLabel(alpha, beta);
+
+	}
+}
+
+void LoadImage(){
+
+}
+
+void InitLabel(){
+	INT32T i, l, minCost, minCostLabel;
+	for (i = 0; i < MAXPIXEL; i++){
+		minCost = INF;
+		minCostLabel = 0;
+		for (l = 0; l < numLabels; l++) if (ComputeDataCost(i, l) < minCost){
+			minCost = ComputeDataCost(i, l);
+			minCostLabel = l;
+		}
+		lb[i] = minCostLabel;
+	}
+}
+
+void SwapOperation(INT32T count){
+	INT32T newEnergy, oldEnergy;
+	InitMap();
+	BuildMap();
+	InitLabel();
+	newEnergy = ComputeEnergy();
+	oldEnergy = newEnergy + 1;
+	while ((oldEnergy != newEnergy) && (count-- > 0)){
+		oldEnergy = newEnergy;
+		AlphaBetaSwap();
+		newEnergy = ComputeEnergy();
+	}
+}
+
+void OutputSegmentationLabel(){
+
+}
+
+//void main(){
+void ImageSegment(){
+	numLabels = 3;
+	w = C;
+	h = R;
+	SwapOperation(6);
+	OutputSegmentationLabel();
+}
